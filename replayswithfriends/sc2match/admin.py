727aa0e25589ab3c447bd1f94120eb1f56a03ee7
Issac Kelly <issac.kelly@gmail.com>
2012-05-02 15:00:08 -0700

    new admin

--- replayswithfriends/sc2match/admin.py
+++ replayswithfriends/sc2match/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-from .models import Match, PlayerResult, Map
+from .models import Match, PlayerResult, Map, Player
+
+
+class PlayerResultInline(admin.StackedInline):
+    model = PlayerResult
+
+
+class PlayerInline(admin.StackedInline):
+    model = Player
+
 
 class MatchAdmin(admin.ModelAdmin):
     list_display = ['id', '__unicode__']
@@ -7,13 +16,20 @@ class MatchAdmin(admin.ModelAdmin):
         'mapfield',
         'duration',
     ]
+    inlines = [PlayerResultInline]
+
 
 class PlayerResultAdmin(admin.ModelAdmin):
     list_display = ['player', 'match']
+    inlines = [PlayerInline]
+
+
+class PlayerAdmin(admin.ModelAdmin):
+    pass
 
 
 
 admin.site.register(PlayerResult, PlayerResultAdmin)
 admin.site.register(Match, MatchAdmin)
 admin.site.register(Map)
-
+admin.site.register(Player, PlayerAdmin)
